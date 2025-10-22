9c4f3f244ca019315f5b7230282fb556a224a0dc
anibaL <a88anibal@gmail.com>
2025-10-22 16:49:05 +0200

    Función agregar VJ a la base de datos [OK]

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import com.example.data.ConexionDB;
+
 import com.example.utils.MenuPrincipal;
 
 public class Main {
@@ -8,9 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        ConexionDB conexionDB = new ConexionDB();
-        conexionDB.infoConnection();
         MenuPrincipal menuPrincipal = new MenuPrincipal();
 
         menuPrincipal.miMenu();
